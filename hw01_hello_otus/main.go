ee6cfcf281525e0ce49f6b2911a5ccff32dc909b
Vadim Vaskevich <vadskev@ya.ru>
2024-02-14 18:02:19 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	line := "Hello, OTUS!"
+	lineReverse := reverse.String(line)
+	fmt.Println(lineReverse)
 }
