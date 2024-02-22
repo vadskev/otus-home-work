413b36f06adcb83c52472c082c3c30ff6045d01f
Vadim Vaskevich <vadskev@ya.ru>
2024-02-22 13:06:21 +0300

    Fix problem with a single backslash

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -18,21 +18,28 @@ func Unpack(line string) (string, error) {
 		return "", nil
 	}
 
+	isBackslash := func(lineRune []rune, index *int) bool {
+		if lineRune[*index] == '\\' && (unicode.IsDigit(lineRune[*index+1]) || lineRune[*index+1] == '\\') {
+			*index++
+			return true
+		}
+		return false
+	}
+
 	var bufChars strings.Builder
 	lineArray := []rune(line)
 
 	for i := 0; i < len(lineArray); i++ {
-		if unicode.IsDigit(lineArray[i]) {
-			if (unicode.IsDigit(lineArray[i+1]) || lineArray[i+1] == '\\') && lineArray[i] == '\\' {
-				i++
-			} else {
-				return "", ErrInvalidString
-			}
+		if !isBackslash(lineArray, &i) && unicode.IsDigit(lineArray[i]) {
+			return "", ErrInvalidString
 		}
-
 		if i < len(lineArray)-1 {
 			if unicode.IsDigit(lineArray[i+1]) {
-				numRepeat, _ := strconv.Atoi(string(lineArray[i+1]))
+				numRepeat, err := strconv.Atoi(string(lineArray[i+1]))
+				if err != nil {
+					return "", ErrInvalidString
+				}
+
 				for j := 0; j < numRepeat; j++ {
 					bufChars.WriteRune(lineArray[i])
 				}
