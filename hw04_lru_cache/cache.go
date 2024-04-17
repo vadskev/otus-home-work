d546dbfe8e7877dcccc2ebcf6452908e29712017
Vadim Vaskevich <vadskev@ya.ru>
2024-04-17 12:34:33 +0300

    fix: add mutex to cache.go > Clear()

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -58,6 +58,8 @@ func (cache *lruCache) Get(key Key) (interface{}, bool) {
 }
 
 func (cache *lruCache) Clear() {
+	cache.mutex.Lock()
+	defer cache.mutex.Unlock()
 	cache.queue = NewList()
 	cache.items = make(map[Key]*ListItem, cache.capacity)
 }
