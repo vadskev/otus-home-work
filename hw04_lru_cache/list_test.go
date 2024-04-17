dba69d403a657fa462632f3c383caf407bdce39c
Vadim Vaskevich <vadskev@ya.ru>
2024-04-17 13:01:59 +0300

    fix: add list_test.go

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -48,4 +48,78 @@ func TestList(t *testing.T) {
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
 	})
+
+	t.Run("complexTwo", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(11)
+		l.PushBack(22)
+		l.PushBack(33)
+
+		l.Remove(l.Back())
+		l.Remove(l.Front())
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 22, l.Front().Value)
+
+		/**/
+		l.Remove(l.Front())
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
+		require.Equal(t, 0, l.Len())
+
+		/**/
+		l.PushFront(300)
+		l.PushFront(200)
+		l.PushFront(100)
+
+		items1 := make([]int, 0, l.Len())
+		for i := l.Front(); i != nil; i = i.Next {
+			items1 = append(items1, i.Value.(int))
+		}
+		require.Equal(t, []int{100, 200, 300}, items1)
+
+		/**/
+		l.MoveToFront(l.Back())
+		require.Nil(t, l.Front().Prev)
+		require.Nil(t, l.Back().Next)
+
+		/**/
+		l.MoveToFront(l.Back())
+		require.Nil(t, l.Front().Prev)
+		require.Nil(t, l.Back().Next)
+
+		/**/
+		items2 := make([]int, 0, l.Len())
+		for i := l.Front(); i != nil; i = i.Next {
+			items2 = append(items2, i.Value.(int))
+		}
+		require.Equal(t, []int{200, 300, 100}, items2)
+
+		/**/
+		l.Remove(l.Back())
+		l.Remove(l.Front())
+		require.Equal(t, 300, l.Back().Value)
+		require.Equal(t, 1, l.Len())
+
+		/**/
+		l.MoveToFront(l.Back())
+		require.Nil(t, l.Front().Prev)
+		require.Nil(t, l.Back().Next)
+		require.Equal(t, 300, l.Back().Value)
+
+		/**/
+		l.PushBack(44)
+		l.PushBack(55)
+		l.MoveToFront(l.Front().Next)
+		require.Nil(t, l.Front().Prev)
+		require.Nil(t, l.Back().Next)
+
+		/**/
+		items3 := make([]int, 0, l.Len())
+		for i := l.Front(); i != nil; i = i.Next {
+			items3 = append(items3, i.Value.(int))
+		}
+		require.Equal(t, []int{44, 300, 55}, items3)
+	})
 }
